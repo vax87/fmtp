d542fe86c0bbc13b591470f528799424c16c9203
sasha <vakhonin.a.n@topaz-atcs.com>
2020-06-30 17:55:30 +0300

    test

--- chief_channel/ChiefChannelServer.go
+++ chief_channel/ChiefChannelServer.go
@@ -703,20 +703,20 @@ func (cc *ChiefChannelServer) startChannelContainer(chSett channel_settings.Chan
 		logger.PrintfInfo("Запущен docker контейнер %s.", curContainerName)
 	}
 
-	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	//statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
 
 	for {
 		select {
-		case cntErr := <-errCh:
-			if cntErr != nil {
-				logger.PrintfErr("Ошибка в работе docker контейнера %s. Ошибка: %v.", curContainerName, cntErr)
-			}
-		case curStatus := <-statusCh:
-			if curStatus.Error != nil {
-				logger.PrintfErr("Получен статус docker контейнера %s. ОшибкаЖ %v. Код: %v.", curContainerName, curStatus.Error.Message, curStatus)
-			} else {
-				logger.PrintfErr("Получен статус docker контейнера %s. Код: %v.", curContainerName, curStatus)
-			}
+		// case cntErr := <-errCh:
+		// 	if cntErr != nil {
+		// 		logger.PrintfErr("Ошибка в работе docker контейнера %s. Ошибка: %v.", curContainerName, cntErr)
+		// 	}
+		// case curStatus := <-statusCh:
+		// 	if curStatus.Error != nil {
+		// 		logger.PrintfErr("Получен статус docker контейнера %s. ОшибкаЖ %v. Код: %v.", curContainerName, curStatus.Error.Message, curStatus)
+		// 	} else {
+		// 		logger.PrintfErr("Получен статус docker контейнера %s. Код: %v.", curContainerName, curStatus)
+		// 	}
 
 		case <-killChan:
 			logger.PrintfInfo("Команда завершить docker контейнер %s.", curContainerName)
